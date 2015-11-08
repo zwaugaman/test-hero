eb8dc10f4d43fad3d150a08b47645665c071248f
zwaugaman <zwaugaman@gmail.com>
2015-11-08 00:22:50 -0500

    modified heroku

--- emailer/wsgi.py
+++ emailer/wsgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emailer.settings")
 
