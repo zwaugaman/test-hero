1c6cc7cae70122652829de7535f728098274c010
zwaugaman <zwaugaman@gmail.com>
2015-11-08 00:39:20 -0500

    debug on

--- emailer/settings.py
+++ emailer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "{{ secret_key }}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 
 # Application definition
