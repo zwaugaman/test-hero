dd816e738bb94cb2daa7864147a3cc2d5c733462
zwaugaman <zwaugaman@gmail.com>
2015-11-08 00:45:02 -0500

    polls added

--- polls/models.py
+++ polls/models.py
@@ -5,11 +5,9 @@ from django.utils import timezone
 
 from django.db import models
 
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
