a755662aa052f70ec8002a0b374887bd2ec83864
zwaugaman <zwaugaman@gmail.com>
2015-11-08 21:01:01 -0500

    updated admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -9,3 +9,5 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
