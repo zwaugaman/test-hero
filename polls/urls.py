c2b725e1f8bd2eac064b9b3aa141b97a9315b2c3
zwaugaman <zwaugaman@gmail.com>
2015-11-08 23:22:49 -0500

    added polls urls

--- polls/urls.py
+++ polls/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 
     # ex: /polls/5/results/
-    url(r'^(?P,question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
