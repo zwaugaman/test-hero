8b0c9d62623ef39b3821176af90e5d31006d05cf
zwaugaman <zwaugaman@gmail.com>
2015-11-08 21:14:20 -0500

    updated admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-class ChoiceInline(admin.TabulaarInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
