7ef68bdb739f8823b14f324ba4a5a554886e91d0
zwaugaman <zwaugaman@gmail.com>
2015-11-10 00:02:03 -0500

    used generic views

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     # ex: /polls/5/
-    url(r'^(?P<pk>[0-9]+)/$', views.IndexView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     # ex: /polls/5/results/
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
