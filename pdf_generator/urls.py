d44831e3282f78719dbe48da326d6a1d15f6bbe8
zwaugaman <zwaugaman@gmail.com>
2015-11-20 23:21:44 -0500

    edited pdf_generator url

--- pdf_generator/urls.py
+++ pdf_generator/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.some_view, name='index'),
 
     # ex: /pdf/button/
-    url(r'^/button/$', views.button_view.as_view(), name='button'),
+    url(r'^button/$', views.button_view.as_view(), name='button'),
 
 ]
