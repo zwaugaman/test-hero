fe326382002f778f25ba5ae1e0fdcf9ae3b9797a
zwaugaman <zwaugaman@gmail.com>
2015-11-21 00:07:29 -0500

    updated pdf generator form and link

--- pdf_generator/urls.py
+++ pdf_generator/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex" /pdf/
-    url(r'^$', views.some_view, name='index'),
+    url(r'^generate', views.some_view, name='index'),
 
     # ex: /pdf/button/
     url(r'^button/$', views.button_view.as_view(), name='button'),
