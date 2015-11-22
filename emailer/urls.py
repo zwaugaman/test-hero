7108687f14956b2ca14c25b06097c4de82b2ea04
zwaugaman <zwaugaman@gmail.com>
2015-11-22 01:18:03 -0500

    added s3 direct namespace

--- emailer/urls.py
+++ emailer/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
 
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^s3direct/', include('s3direct.urls')),
+    url(r'^s3direct/', include('s3direct.urls', namespace="s3direct")),
     url(r'^cat/', include('cat.urls')),
     url(r'^pdf/', include('pdf_generator.urls', namespace="pdf")),
 )
